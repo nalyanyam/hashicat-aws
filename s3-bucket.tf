ff7cd40314d70d30e764ef64be465e8458cfde16
Mark Nalyanya <marknalyanya@yahoo.com>
2023-02-24 12:55:07 +0000

    Update S3 bucket name

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source  = "app.terraform.io/wmp-mnalyanya/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "my-s3-bucket"
+  bucket = "mn-s3-bucket1"
   acl    = "private"
 
   versioning = {
