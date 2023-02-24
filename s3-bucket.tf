a0d00414c0b1bafdb870c0b584627b2526308c08
Mark Nalyanya <marknalyanya@yahoo.com>
2023-02-24 13:22:04 +0000

    Remove bucket_prefix, conflicts with bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,6 @@ module "s3_bucket" {
   source  = "app.terraform.io/wmp-mnalyanya/s3-bucket/aws"
   version = "2.8.0"
   bucket = "mn-s3-bucket1"
-  bucket_prefix = "marknalyanya"
   acl    = "private"
 
   versioning = {
